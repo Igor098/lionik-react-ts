984221e5b584315c9162a135af77b6d2d56c6389
Igor098 <igor.novozhilov2018@gmail.com>
2024-12-02 02:27:50 +0300

    feat: начал работать над проектом. Сделал шапку сайта с адаптивом

--- src/components/Navigation/NavigationMobile.tsx
+++ src/components/Navigation/NavigationMobile.tsx
@@ -21,7 +21,7 @@ const NavigationMobile = () => {
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
                         transition={{ duration: 0.2 }}
-                        className="nav left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 bg-neutral-950 border-b border-b-white/20">
+                        className="nav">
                         <ul className="nav__list list-reset">
                             {routes.map((route, index) => {
                                 const { title, href } = route;
